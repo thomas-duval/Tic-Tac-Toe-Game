475d96146637b2aa8fff45d7894b6d7df0a4921d
thomas-duval <thomas.duval@opmbx.org>
2016-05-12 09:02:56 +0200

    Adding grid + modal

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,52 @@
-console.log('\'Allo \'Allo!');
+var game = (function () {
+    // variable
+    var
+        grid = [[, , ,], [, , ,], [, , ,]],
+        cpu, player, timeOutModal;
+
+    // Cache DOM
+    var
+        $grid = $('#grid'),
+        $modal = $('#myModal'),
+        $button = $('.modal-footer').find('.sideButton');
+
+    // Bind events
+    $button.on('click', setSide);
+
+
+    function delayedModal() {
+        timeOutModal = window.setTimeout(modal, 1000);
+    }
+
+    function modal() {
+        $modal.modal('show');
+    }
+
+    function setSide() {
+        var data = $(this).html();
+        if (data === 'X') {
+            player = 'X';
+            cpu = 'O';
+        } else {
+            player = 'O';
+            cpu = 'X';
+        }
+        console.log(('Player = ' + player + ' & CPU = ' + cpu));
+    }
+
+
+
+
+    console.log($button);
+
+    delayedModal();
+
+    return ;
+
+
+
+
+
+})();
+
+
